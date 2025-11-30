8d3bb8522b73c776afcdf60d971b9c88cec5ce90
qiaoli-astro <1614185931@qq.com>
2025-11-30 18:12:23 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards', 'links']
+const section_names = ['home', 'publications', 'awards', 'talks', 'links']
 
 
 window.addEventListener('DOMContentLoaded', event => {
